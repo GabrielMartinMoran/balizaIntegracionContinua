aea86df4e7b4c9023484dc7c1cb611d655b9bab5
Catero <cybercat_666_78@hotmail.com>
2018-10-20 22:55:46 -0300

    creado Led

---
+++ utils/leds/Led.py
@@ -0,0 +1,22 @@
+import machine
+
+
+class NumeroInvalidoException(Exception):
+
+    def __init__(self, mensaje):
+        self.mensaje = mensaje
+
+
+class Led:
+    def __init__(self, pin_led):
+        self.pin_led = machine.PWM(machine.Pin(pin_led))
+        self.set(0)
+
+    def set(self, led):
+        self.led = int(led)
+        self.pin_led.duty(self.__map__(self.led))
+
+    def __map__(self, n):
+        if(n < 0 or n > 255):
+            raise NumeroInvalidoException("El valor debe estar entre 0 y 255")
+        return int((float(n) / 255) * 1023)
