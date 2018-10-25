1b793c77266edfe56843cadfbc1bcc68d64ede75
Gabriel Martin Moran <moran.gabriel.95@gmail.com>
2018-10-25 20:57:35 -0300

    Modificado boot.py y agregado colores

--- baliza/boot.py
+++ baliza/boot.py
@@ -1,3 +1,4 @@
+"""
 import network
 import webrepl
 print("\n----> INICIANDO webrepl\n")
@@ -6,3 +7,33 @@ print("\n----> INICIANDO AP\n")
 ap_if = network.WLAN(network.AP_IF)
 ap_if.active(True)
 print("\n\t~~ BIENVENIDO A LA BALIZA ~~\n")
+"""
+# This file is executed on every boot (including wake-boot from deepsleep)
+# import esp
+# esp.osdebug(None)
+import network
+import webrepl
+import time
+
+
+webrepl.start()
+ap_if = network.WLAN(network.AP_IF)
+ap_if.active(True)
+ap_if.config(essid='ESP32', authmode=network.AUTH_WPA_WPA2_PSK, password='micropython')
+sta_if = network.WLAN(network.STA_IF)
+ap_if.active(True)
+for i in range(10):
+    sta_if.connect('CyberGames', '')
+    if(sta_if.isconnected()):
+        break
+    time.sleep(1)
+
+print("Conectado a CyberGames ", sta_if.isconnected())
+
+for i in range(10):
+    sta_if.connect('AP', 'Passw0rd')
+    if(sta_if.isconnected()):
+        break
+    time.sleep(1)
+
+print("Conectado a AP ", sta_if.isconnected())
---
+++ utils/travisClient/Colores.py
@@ -0,0 +1,4 @@
+#Definimos el diccionario de colores
+ROJO = (255,0,0)
+VERDE = (0,255,0)
+AZUL = (0,0,255)
