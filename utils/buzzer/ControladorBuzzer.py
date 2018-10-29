5c6dce1e9f2643a548c014235698ce2c17dc7ef3
Catero <cybercat_666_78@hotmail.com>
2018-10-29 06:30:57 -0300

    ManejadorBuzzer, ControladorBuzzer funcionando

--- utils/buzzer/ControladorBuzzer.py
+++ utils/buzzer/ControladorBuzzer.py
@@ -17,13 +17,14 @@ class ControladorBuzzer:
     def __init__(self, pin_buzzer):
         self.pin_buzzer = machine.PWM(machine.Pin(pin_buzzer))
         self.set_intensidad(0)
+        self.set_frecuencia(1)
 
     def set_intensidad(self, intensidad):
         if(intensidad < 0 or intensidad > 1023):
             raise IntensidadPWMInvalidaException("La intensidad PWM debe estar entre 0 y 1023")
-        self.pin_led.duty(intensidad)
+        self.pin_buzzer.duty(intensidad)
 
     def set_frecuencia(self, frecuencia):
-        if(frecuencia < 0 or frecuencia > 5000):
-            raise IntensidadPWMInvalidaException("La frecuencia PWM debe estar entre 0 y 5000")
-        self.pin_led.freq(frecuencia)
+        if(frecuencia < 1 or frecuencia > 5000):
+            raise IntensidadPWMInvalidaException("La frecuencia PWM debe estar entre 1 y 5000")
+        self.pin_buzzer.freq(frecuencia)
