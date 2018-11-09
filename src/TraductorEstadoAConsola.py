664819c3ea0eec65046997e7ec6566ebe9a95083
Germán Torós <cybercat_666_78@hotmail.com>
2018-11-09 06:23:10 -0300

    refactor

--- src/TraductorEstadoAConsola.py
+++ src/TraductorEstadoAConsola.py
@@ -2,8 +2,10 @@ from EstadoBuild import EstadoBuild
 
 class TraductorEstadoAConsola:
 
+    __mensaje_de_cambio_de_estado = "CAMBIO DE ESTADO DEL BUILD A:"
+
     def __init__(self):
         pass
     
     def set_estado(self, estado):
-        print("CAMBIO DE ESTADO DEL BUILD A:", estado)
+        print(self.__mensaje_de_cambio_de_estado, estado)
